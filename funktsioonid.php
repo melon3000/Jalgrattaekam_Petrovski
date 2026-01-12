989cf9c96ea6463a17e260b34c1c6048956c428b
melon3000 <milan.petrovski8@gmail.com>
2026-01-12 15:12:12 +0200

    muuda funktsioonid.php

--- funktsioonid.php
+++ funktsioonid.php
@@ -2,11 +2,11 @@
 global $yhendus;
 require_once("konf.php");
 
-function registreermine($eesnimi, $perekonnanimi){
+function registrerimine($eesnimi,$perekonnanimi){
     global $yhendus;
 
     if(empty($eesnimi) || empty($perekonnanimi) || is_numeric($eesnimi) || is_numeric($perekonnanimi)){
-        header("Location: ".$_SERVER[PHP_SELF]."?viga=1"); exit();
+        header("Location: $_SERVER[PHP_SELF]?viga=1"); exit();
     }
     $kask=$yhendus->prepare(
         "INSERT INTO jalgrattaeksam(eesnimi, perekonnanimi) VALUES (?, ?)");
@@ -14,35 +14,71 @@ function registreermine($eesnimi, $perekonnanimi){
     $kask->execute();
     $yhendus->close();
 }
-// teooria
-function teooriatulemus($teooriatulemus, $id)
+function teoriatulemus($teooriatulemus, $id)
 {
-global $yhendus;
-$kask=$yhendus->prepare(
-"UPDATE jalgrattaeksam SET teooriatulemus=? WHERE id=?");
-$kask->bind_param("ii", $teooriatulemus, $id); $kask->execute();
-}
+    global $yhendus;
+    $kask=$yhendus->prepare(
+        "UPDATE jalgrattaeksam SET teooriatulemus=? WHERE id=?");
+    $kask->bind_param("ii", $teooriatulemus, $id); $kask->execute();}
 
 function kuvatabel()
 {
-global $yhendus;
-$kask=$yhendus->prepare("SELECT id, eesnimi, perekonnanimi, teooriatulemus FROM jalgrattaeksam WHERE teooriatulemus<10");
-$kask->bind_result($id, $eesnimi, $perekonnanimi, $teooriatulemus);
-$kask->execute();
+    global $yhendus;
+    $kask=$yhendus->prepare("SELECT id, eesnimi, perekonnanimi,teooriatulemus FROM jalgrattaeksam WHERE teooriatulemus<10");
+    $kask->bind_result($id, $eesnimi, $perekonnanimi, $teooriatulemus );
+    $kask->execute();
 
-while($kask->fetch()){
-echo "
-<tr>
-    <td>$eesnimi</td>
-    <td>$perekonnanimi</td>
-    <td><form action=''>
-            <input type='hidden' name='id' value='$id' />
-            <input type='text' name='teooriatulemus' />
-            <input type='submit' value='Sisesta tulemus' />
-            <td>$teooriatulemus</td>
-        </form>
-    </td>
-</tr>
-";
+    while($kask->fetch()){
+        echo " 
+ <tr> 
+ <td>$eesnimi</td> 
+ <td>$perekonnanimi</td> 
+ <td><form action=''> 
+ <input type='hidden' name='id' value='$id' /> 
+ <input type='text' name='teooriatulemus' />
+ <input type='submit' value='Sisesta tulemus' /> 
+ <td>$teooriatulemus</td> 
+ </form> 
+ </td> 
+</tr> 
+ ";
+    }
 }
+
+function slaalomtabel()
+{
+    global $yhendus;
+    $kask=$yhendus->prepare("SELECT id, eesnimi, perekonnanimi, slaalom FROM jalgrattaeksam WHERE teooriatulemus>=9 AND slaalom=-1");
+    $kask->bind_result($id, $eesnimi, $perekonnanimi, $slaalom);
+    $kask->execute();
+
+    while($kask->fetch()){
+        echo " 
+ <tr> 
+ <td>$eesnimi</td> 
+ <td>$perekonnanimi</td> 
+ <td> 
+ <a href='?korras_id=$id'>Korras</a>
+ <a href='?vigane_id=$id'>Ebaõnnestunud</a> 
+ </td> 
+</tr> 
+ ";
+    }
 }
+function slaalomKorras($id){
+    global $yhendus;
+    $kask=$yhendus->prepare(
+        "UPDATE jalgrattaeksam SET slaalom=1 WHERE id=?");
+    $kask->bind_param("i", $id);
+    $kask->execute();
+}
+function slaalomVigane($id){
+    global $yhendus;
+    $kask=$yhendus->prepare(
+        "UPDATE jalgrattaeksam SET slaalom=-1 WHERE id=?");
+    $kask->bind_param("i", $id);
+    $kask->execute();
+
+}
+?>
+
