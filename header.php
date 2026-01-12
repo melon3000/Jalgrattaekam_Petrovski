f3e69b45d7e36a5d5b1fd6de2bc94e9165a11337
melon3000 <milan.petrovski8@gmail.com>
2026-01-12 14:41:22 +0200

    Lisatud header.php

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
     <h1>Jalgrattaeksam Admin</h1>
 </header>
 <nav>
-    <a href="../registreerimine.php">Registreerimine</a>
-    <a href="../Teooriaeksam.php">Teooriaeksam</a>
-    <a href="../Slaalom.php">Slaalom</a>
-    <a href="../Ringtee.php">Ringtee</a>
-    <a href="../Tänav.php">Tänavasõit</a>
-    <a href="../Lubadeleht.php">Lubade leht</a>
+    <a href="registreerimine.php">Registreerimine</a>
+    <a href="Teooriaeksam.php">Teooriaeksam</a>
+    <a href="Slaalom.php">Slaalom</a>
+    <a href="Ringtee.php">Ringtee</a>
+    <a href="Tänav.php">Tänavasõit</a>
+    <a href="Lubadeleht.php">Lubade leht</a>
 </nav>
 
