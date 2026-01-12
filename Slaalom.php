d5e766bf63bc05e60955fdf4cd83a22fd38dc3cb
melon3000 <milan.petrovski8@gmail.com>
2026-01-12 15:11:34 +0200

    muuda slaloom.php

--- Slaalom.php
+++ Slaalom.php
@@ -1,20 +1,13 @@
 <?php
-require_once("konf.php");
+require_once("funktsioonid.php");
 global $yhendus;
 if(!empty($_REQUEST["korras_id"])){
-    $kask=$yhendus->prepare(
-        "UPDATE jalgrattaeksam SET slaalom=1 WHERE id=?");
-    $kask->bind_param("i", $_REQUEST["korras_id"]);
-    $kask->execute();
+    slaalomKorras();
 }
 if(!empty($_REQUEST["vigane_id"])){
-    $kask=$yhendus->prepare(
-        "UPDATE jalgrattaeksam SET slaalom=2 WHERE id=?");
-    $kask->bind_param("i", $_REQUEST["vigane_id"]);
-    $kask->execute();
+    slaalomVigane();
 }
-$kask=$yhendus->prepare("SELECT id, eesnimi, perekonnanimi   FROM jalgrattaeksam WHERE teooriatulemus>9 AND slaalom=-1");  $kask->bind_result($id, $eesnimi, $perekonnanimi);
-$kask->execute();
+
 ?>
 <!doctype html>
 <html>
@@ -23,24 +16,13 @@ $kask->execute();
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
+<?php require("header.php");?>
 <h1>Slaalom</h1>
 <table>
     <?php
-    include("header.php");
-
-    while($kask->fetch()){
-        echo " 
- <tr> 
- <td>$eesnimi</td> 
- <td>$perekonnanimi</td> 
- <td> 
- <a href='?korras_id=$id'>Korras</a>
- <a href='?vigane_id=$id'>Ebaõnnestunud</a> 
- </td> 
-</tr> 
- ";
-    }
+        kuvatabel();
     ?>
 </table>
+<?php require("footer.php");?>
 </body>
 </html>
