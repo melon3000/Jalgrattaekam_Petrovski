35d7f1eb2dc2bbff8d9b04ef31aef70b66e91e79
melon3000 <milan.petrovski8@gmail.com>
2026-01-12 12:55:43 +0200

    Lisatud fail Slaalom.php kus võib panna õpilastele korras / ebaõnnestus staatus

---
+++ Slaalom.php
@@ -0,0 +1,43 @@
+<?php
+require_once("konf.php");
+global $yhendus;
+if(!empty($_REQUEST["korras_id"])){
+    $kask=$yhendus->prepare(
+        "UPDATE jalgrattaeksam SET slaalom=1 WHERE id=?");
+    $kask->bind_param("i", $_REQUEST["korras_id"]);
+    $kask->execute();
+}
+if(!empty($_REQUEST["vigane_id"])){
+    $kask=$yhendus->prepare(
+        "UPDATE jalgrattaeksam SET slaalom=2 WHERE id=?");
+    $kask->bind_param("i", $_REQUEST["vigane_id"]);
+    $kask->execute();
+}
+$kask=$yhendus->prepare("SELECT id, eesnimi, perekonnanimi   FROM jalgrattaeksam WHERE teooriatulemus>=9 AND slaalom=-1");  $kask->bind_result($id, $eesnimi, $perekonnanimi);
+$kask->execute();
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Slaalom</title>
+</head>
+<body>
+<h1>Slaalom</h1>
+<table>
+    <?php
+    while($kask->fetch()){
+        echo " 
+ <tr> 
+ <td>$eesnimi</td> 
+ <td>$perekonnanimi</td> 
+ <td> 
+ <a href='?korras_id=$id'>Korras</a>
+ <a href='?vigane_id=$id'>Ebaõnnestunud</a> 
+ </td> 
+</tr> 
+ ";
+    }
+    ?>
+</table>
+</body>
+</html>
